cd26b1f5f826c1fe61386620943b1c2a76831a1f
En3Tho <37334640+En3Tho@users.noreply.github.com>
2021-10-06 13:39:51 +0300

    Added Async versions of Validated Bumped Validation version to 1.0.3

--- FSharpExtensions/En3Tho.FSharp.Validation/BasicTypes.fs
+++ FSharpExtensions/En3Tho.FSharp.Validation/BasicTypes.fs
@@ -4,6 +4,7 @@ open System
 open System.Runtime.CompilerServices
 open System.Threading.Tasks
 open En3Tho.FSharp.Extensions
+open FSharp.Control.Tasks
 
 // TODO: ResourceManagement, Localization
 
@@ -28,9 +29,8 @@ type IValidator<'value> =
     abstract member Validate: 'value -> ExnResult<'value>
     abstract member ValidateAggregate: 'value -> EResult<'value, AggregateException>
 
-type [<Struct; IsReadOnly>] NewCtorValidatorValidated<'value, 'validator when 'validator :> IValidator<'value>
-                                                                and 'validator: (new: unit -> 'validator)
-                                                                and 'validator :> IValidator<'value>>
+type [<Struct; IsReadOnly>] NewCtorValidatorValidated<'value, 'validator when 'validator: (new: unit -> 'validator)
+                                                                          and 'validator :> IValidator<'value>>
 #if DEBUG
     private (value: 'value, wasValidated: bool) =
     private new(value) = Validated10(value, true)
@@ -83,16 +83,16 @@ type [<Struct; IsReadOnly>] NewCtorValidatorValidated<'value, 'validator when 'v
     member this.MapMakeAggregate (map: 'value -> 'value) =
         NewCtorValidatorValidated<'value, 'validator>.MakeAggregate(map this.Value)
 
-    static member TryAggregate (value: 'value voption) =
-        match value with
+    static member TryAggregate (valueOption: 'value voption) =
+        match valueOption with
         | ValueNone -> ValueNone |> Ok
         | ValueSome value ->
             match NewCtorValidatorValidated<'value, 'validator>.TryAggregate value with
             | Ok value -> value |> ValueSome |> Ok
             | Error err -> Error err
 
-    static member MakeAggregate (value: 'value voption) =
-        NewCtorValidatorValidated<'value, 'validator>.TryAggregate(value) |> EResult.unwrap
+    static member MakeAggregate (valueOption: 'value voption) =
+        NewCtorValidatorValidated<'value, 'validator>.TryAggregate(valueOption) |> EResult.unwrap
 
 type [<Struct; IsReadOnly>] InstanceValidatorValidated<'value, 'validator when 'validator :> IValidator<'value>>
 #if DEBUG
@@ -145,15 +145,166 @@ type [<Struct; IsReadOnly>] InstanceValidatorValidated<'value, 'validator when '
     member this.MapMakeAggregate (map: 'value -> 'value) =
         InstanceValidatorValidated<'value, 'validator>.MakeAggregate(map this.Value, this.Validator)
 
-    static member TryAggregate(value: 'value voption, validator: 'validator) =
-        match value with
+    static member TryAggregate(valueOption: 'value voption, validator: 'validator) =
+        match valueOption with
         | ValueNone -> ValueNone |> Ok
         | ValueSome value ->
             match InstanceValidatorValidated<'value, 'validator>.TryAggregate(value, validator) with
             | Ok value -> value |> ValueSome |> Ok
             | Error err -> Error err
 
-    static member MakeAggregate(value: 'value voption, validator: 'validator) =
-        InstanceValidatorValidated<'value, 'validator>.TryAggregate(value, validator) |> EResult.unwrap
+    static member MakeAggregate(valueOption: 'value voption, validator: 'validator) =
+        InstanceValidatorValidated<'value, 'validator>.TryAggregate(valueOption, validator) |> EResult.unwrap
+
+type [<Struct; IsReadOnly>] NewCtorAsyncValidatorValidated<'value, 'validator when 'validator: (new: unit -> 'validator)
+                                                                               and 'validator :> IAsyncValidator<'value>>
+#if DEBUG
+    private (value: 'value, wasValidated: bool) =
+    private new(value) = Validated10(value, true)
+#else
+    private (value: 'value, validator: 'validator) =
+    member _.Value = value
+#endif
+    member internal _.Validator = validator
+
+    override this.ToString() = value.ToString()
+
+    static member Try value = vtask {
+        let validator = (new 'validator())
+        match! validator.Validate value with
+        | Ok value -> return NewCtorAsyncValidatorValidated(value, validator) |> Ok
+        | Error err -> return Error err
+    }
+
+    static member Make (value: 'value) = vtask {
+        let! result = NewCtorAsyncValidatorValidated<'value, 'validator>.Try(value)
+        return result |> EResult.unwrap
+    }
+
+    member this.MapTry (map: 'value -> 'value) =
+        NewCtorAsyncValidatorValidated<'value, 'validator>.Try(map this.Value)
+
+    member this.MapMake (map: 'value -> 'value) =
+        NewCtorAsyncValidatorValidated<'value, 'validator>.Make(map this.Value)
+
+    static member Try (valueOption: 'value voption) = vtask {
+        match valueOption with
+        | ValueNone -> return ValueNone |> Ok
+        | ValueSome value ->
+            match! NewCtorAsyncValidatorValidated<'value, 'validator>.Try value with
+            | Ok value -> return value |> ValueSome |> Ok
+            | Error err -> return Error err
+    }
+
+    static member Make (valueOption: 'value voption) = vtask {
+        let! result = NewCtorAsyncValidatorValidated<'value, 'validator>.Try(valueOption)
+        return result |> EResult.unwrap
+    }
+
+    static member TryAggregate value = vtask {
+        let validator = (new 'validator())
+        match! validator.ValidateAggregate value with
+        | Ok value -> return NewCtorAsyncValidatorValidated(value, validator) |> Ok
+        | Error err -> return Error err
+    }
+
+    static member MakeAggregate (value: 'value) = vtask {
+        let! result = NewCtorAsyncValidatorValidated<'value, 'validator>.TryAggregate(value)
+        return result |> EResult.unwrap
+    }
+
+    member this.MapTryAggregate (map: 'value -> 'value) =
+        NewCtorAsyncValidatorValidated<'value, 'validator>.TryAggregate(map this.Value)
+
+    member this.MapMakeAggregate (map: 'value -> 'value) =
+        NewCtorAsyncValidatorValidated<'value, 'validator>.MakeAggregate(map this.Value)
+
+    static member TryAggregate (valueOption: 'value voption) = vtask {
+        match valueOption with
+        | ValueNone -> return ValueNone |> Ok
+        | ValueSome value ->
+            match! NewCtorAsyncValidatorValidated<'value, 'validator>.TryAggregate value with
+            | Ok value -> return value |> ValueSome |> Ok
+            | Error err -> return Error err
+    }
+
+    static member MakeAggregate (valueOption: 'value voption) = vtask {
+        let! result = NewCtorAsyncValidatorValidated<'value, 'validator>.TryAggregate(valueOption)
+        return result |> EResult.unwrap
+    }
+
+type [<Struct; IsReadOnly>] InstanceAsyncValidatorValidated<'value, 'validator when 'validator :> IAsyncValidator<'value>>
+#if DEBUG
+    private (value: 'value, wasValidated: bool) =
+    private new(value) = Validated10(value, true)
+#else
+    private (value: 'value, validator: 'validator) =
+    member _.Value = value
+#endif
+    member internal _.Validator = validator
+
+    override this.ToString() = value.ToString()
+
+    static member Try(value: 'value, validator: 'validator) = vtask {
+        match! validator.Validate value with
+        | Ok value -> return InstanceAsyncValidatorValidated(value, validator) |> Ok
+        | Error err -> return Error err
+    }
+
+    static member Make(value: 'value, validator: 'validator) = vtask {
+        let! result = InstanceAsyncValidatorValidated<'value, 'validator>.Try(value, validator)
+        return result  |> EResult.unwrap
+    }
+
+    member this.MapTry (map: 'value -> 'value) =
+        InstanceAsyncValidatorValidated<'value, 'validator>.Try(map this.Value, this.Validator)
+
+    member this.MapMake (map: 'value -> 'value) =
+        InstanceAsyncValidatorValidated<'value, 'validator>.Make(map this.Value, this.Validator)
+
+    static member Try(valueOption: 'value voption, validator: 'validator) = vtask {
+        match valueOption with
+        | ValueNone -> return ValueNone |> Ok
+        | ValueSome value ->
+            match! InstanceAsyncValidatorValidated<'value, 'validator>.Try(value, validator) with
+            | Ok value -> return value |> ValueSome |> Ok
+            | Error err -> return Error err
+    }
+
+    static member Make(valueOption: 'value voption, validator: 'validator) = vtask {
+        let! result = InstanceAsyncValidatorValidated<'value, 'validator>.Try(valueOption, validator)
+        return result |> EResult.unwrap
+    }
+
+    static member TryAggregate(value: 'value, validator: 'validator) = vtask {
+        match! validator.ValidateAggregate value with
+        | Ok value -> return InstanceAsyncValidatorValidated(value, validator) |> Ok
+        | Error err -> return Error err
+    }
+
+    static member MakeAggregate(value: 'value, validator: 'validator) = vtask {
+        let! result = InstanceAsyncValidatorValidated<'value, 'validator>.TryAggregate(value, validator)
+        return result |> EResult.unwrap
+    }
+
+    member this.MapTryAggregate (map: 'value -> 'value) =
+        InstanceAsyncValidatorValidated<'value, 'validator>.TryAggregate(map this.Value, this.Validator)
+
+    member this.MapMakeAggregate (map: 'value -> 'value) =
+        InstanceAsyncValidatorValidated<'value, 'validator>.MakeAggregate(map this.Value, this.Validator)
+
+    static member TryAggregate(valueOption: 'value voption, validator: 'validator) = vtask {
+        match valueOption with
+        | ValueNone -> return ValueNone |> Ok
+        | ValueSome value ->
+            match! InstanceAsyncValidatorValidated<'value, 'validator>.TryAggregate(value, validator) with
+            | Ok value -> return value |> ValueSome |> Ok
+            | Error err -> return Error err
+    }
+
+    static member MakeAggregate(valueOption: 'value voption, validator: 'validator) =vtask {
+        let! result = InstanceAsyncValidatorValidated<'value, 'validator>.TryAggregate(valueOption, validator)
+        return result |> EResult.unwrap
+    }
 
 #noward "0043"
--- FSharpExtensions/En3Tho.FSharp.Validation/Modules.fs
+++ FSharpExtensions/En3Tho.FSharp.Validation/Modules.fs
@@ -13,3 +13,17 @@ type ValidatedN =
     static member inline mapTryAggregate map (entity: NewCtorValidatorValidated<'a,'b>) = entity.MapTryAggregate(map)
     static member inline mapMakeAggregate map (entity: NewCtorValidatorValidated<'a,'b>) = entity.MapMakeAggregate(map)
     static member inline value (entity: NewCtorValidatorValidated<'a,'b>) = entity.Value
+
+type AsyncValidatedI =
+    static member inline mapTry map (entity: InstanceAsyncValidatorValidated<'a,'b>) = entity.MapTry(map)
+    static member inline mapMake map (entity: InstanceAsyncValidatorValidated<'a,'b>) = entity.MapMake(map)
+    static member inline mapTryAggregate map (entity: InstanceAsyncValidatorValidated<'a,'b>) = entity.MapTryAggregate(map)
+    static member inline mapMakeAggregate map (entity: InstanceAsyncValidatorValidated<'a,'b>) = entity.MapMakeAggregate(map)
+    static member inline value (entity: InstanceAsyncValidatorValidated<'a,'b>) = entity.Value
+
+type AsyncValidatedN =
+    static member inline mapTry map (entity: NewCtorAsyncValidatorValidated<'a,'b>) = entity.MapTry(map)
+    static member inline mapMake map (entity: NewCtorAsyncValidatorValidated<'a,'b>) = entity.MapMake(map)
+    static member inline mapTryAggregate map (entity: NewCtorAsyncValidatorValidated<'a,'b>) = entity.MapTryAggregate(map)
+    static member inline mapMakeAggregate map (entity: NewCtorAsyncValidatorValidated<'a,'b>) = entity.MapMakeAggregate(map)
+    static member inline value (entity: NewCtorAsyncValidatorValidated<'a,'b>) = entity.Value
