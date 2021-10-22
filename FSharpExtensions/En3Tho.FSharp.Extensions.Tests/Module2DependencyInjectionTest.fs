eb8374f46b733d470bad1391b38c556ca72f3466
En3Tho <37334640+En3Tho@users.noreply.github.com>
2021-10-23 01:00:06 +0300

    Added DI tests. Swapped scanf args

---
+++ FSharpExtensions/En3Tho.FSharp.Extensions.Tests/Module2DependencyInjectionTest.fs
@@ -0,0 +1,36 @@
+﻿namespace En3Tho.FSharp.Extensions.Tests
+
+open Microsoft.Extensions.DependencyInjection
+open Xunit
+
+module Module2 =
+
+    type Startup() =
+        member this.ConfigureServices(services: IServiceCollection) =
+           services
+               .AddSingleton({ Dependency1.Value = "Module2" })
+               .AddSingleton<Dependency2>()
+           |> ignore
+
+    type Test(dep: Dependency1, dep2: Dependency2) =
+
+        [<Fact>]
+        let ``Test that DI works in tests``() =
+            Assert.Equal(dep2.Value.Value, dep.Value)
+            Assert.Equal(dep2.Value.Value, "Module2")
+
+module Module3 =
+
+    type Startup() =
+        member this.ConfigureServices(services: IServiceCollection) =
+           services
+               .AddSingleton({ Dependency1.Value = "Module3" })
+               .AddSingleton<Dependency2>()
+           |> ignore
+
+    type Test(dep: Dependency1, dep2: Dependency2) =
+
+        [<Fact>]
+        let ``Test that DI works in tests``() =
+            Assert.Equal(dep2.Value.Value, dep.Value)
+            Assert.Equal(dep2.Value.Value, "Module3")
---
+++ FSharpExtensions/En3Tho.FSharp.Extensions.Tests/ModuleDependencyInjectionTest.fs
@@ -0,0 +1,18 @@
+﻿module En3Tho.FSharp.Extensions.Tests.Module
+
+open Microsoft.Extensions.DependencyInjection
+open Xunit
+
+type Startup() =
+    member this.ConfigureServices(services: IServiceCollection) =
+       services
+           .AddSingleton({ Dependency1.Value = "Module" })
+           .AddSingleton<Dependency2>()
+       |> ignore
+
+type Test(dep: Dependency1, dep2: Dependency2) =
+
+    [<Fact>]
+    let ``Test that DI works in tests``() =
+        Assert.Equal(dep2.Value.Value, dep.Value)
+        Assert.Equal(dep2.Value.Value, "Module")
