1faa7b09aee6ef5de8cd4f76a72e67bb5fad7572
En3Tho <37334640+En3Tho@users.noreply.github.com>
2021-11-03 14:55:40 +0300

    Added TryAdd*OrFail Dependency Injection helpers

--- FSharpExtensions/En3Tho.Extensions.DependencyInjection/IServiceCollectionExtensions.cs
+++ FSharpExtensions/En3Tho.Extensions.DependencyInjection/IServiceCollectionExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using static System.String;
 
 namespace En3Tho.Extensions.DependencyInjection;
 
@@ -22,7 +24,7 @@ public static class IServiceCollectionExtensions
 
     public static IServiceCollection AddSingletonAsOpenGeneric<TService, TImpl>(this IServiceCollection collection)
         where TService : class
-        where TImpl : TService
+        where TImpl : class, TService
     {
         collection.AddSingleton(typeof(TService).GetGenericTypeDefinition(), typeof(TImpl).GetGenericTypeDefinition());
         return collection;
@@ -37,7 +39,7 @@ public static class IServiceCollectionExtensions
 
     public static IServiceCollection AddScopedAsOpenGeneric<TService, TImpl>(this IServiceCollection collection)
         where TService : class
-        where TImpl : TService
+        where TImpl : class, TService
     {
         collection.AddScoped(typeof(TService).GetGenericTypeDefinition(), typeof(TImpl).GetGenericTypeDefinition());
         return collection;
@@ -52,7 +54,7 @@ public static class IServiceCollectionExtensions
 
     public static IServiceCollection AddTransientAsOpenGeneric<TService, TImpl>(this IServiceCollection collection)
         where TService : class
-        where TImpl : TService
+        where TImpl : class, TService
     {
         collection.AddTransient(typeof(TService).GetGenericTypeDefinition(), typeof(TImpl).GetGenericTypeDefinition());
         return collection;
@@ -67,7 +69,7 @@ public static class IServiceCollectionExtensions
 
     public static IServiceCollection TryAddSingletonAsOpenGeneric<TService, TImpl>(this IServiceCollection collection)
         where TService : class
-        where TImpl : TService
+        where TImpl : class, TService
     {
         collection.TryAddSingleton(typeof(TService).GetGenericTypeDefinition(), typeof(TImpl).GetGenericTypeDefinition());
         return collection;
@@ -82,7 +84,7 @@ public static class IServiceCollectionExtensions
 
     public static IServiceCollection TryAddScopedAsOpenGeneric<TService, TImpl>(this IServiceCollection collection)
         where TService : class
-        where TImpl : TService
+        where TImpl : class, TService
     {
         collection.TryAddScoped(typeof(TService).GetGenericTypeDefinition(), typeof(TImpl).GetGenericTypeDefinition());
         return collection;
@@ -97,9 +99,92 @@ public static class IServiceCollectionExtensions
 
     public static IServiceCollection TryAddTransientAsOpenGeneric<TService, TImpl>(this IServiceCollection collection)
         where TService : class
-        where TImpl : TService
+        where TImpl : class, TService
     {
         collection.TryAddTransient(typeof(TService).GetGenericTypeDefinition(), typeof(TImpl).GetGenericTypeDefinition());
         return collection;
     }
+
+    public static IServiceCollection TryAddSingletonOrFail<TService>(this IServiceCollection collection)
+        where TService : class
+    {
+        var descriptors = collection.Where(d => d.ServiceType == typeof(TService)).ToArray();
+        if (descriptors is { Length: > 0 })
+        {
+            var message = $"Service type {typeof(TService).FullName} is already implemented by {Join(", ", descriptors.Select(d => (d.ImplementationType ?? typeof(TService)).FullName))}";
+            throw new InvalidOperationException(message);
+        }
+
+        return collection.AddSingleton<TService>();
+    }
+
+    public static IServiceCollection TryAddSingletonOrFail<TService, TImpl>(this IServiceCollection collection)
+        where TService : class
+        where TImpl : class, TService
+    {
+        var descriptors = collection.Where(d => d.ServiceType == typeof(TService)).ToArray();
+        if (descriptors is { Length: > 0 })
+        {
+            var message = $"Service type {typeof(TService).FullName} is already implemented by {Join(", ", descriptors.Select(d => (d.ImplementationType ?? typeof(TService)).FullName))}";
+            throw new InvalidOperationException(message);
+        }
+
+        return collection.AddSingleton<TService, TImpl>();
+    }
+
+    public static IServiceCollection TryAddScopedOrFail<TService>(this IServiceCollection collection)
+        where TService : class
+    {
+        var descriptors = collection.Where(d => d.ServiceType == typeof(TService)).ToArray();
+        if (descriptors is { Length: > 0 })
+        {
+            var message = $"Service type {typeof(TService).FullName} is already implemented by {Join(", ", descriptors.Select(d => (d.ImplementationType ?? typeof(TService)).FullName))}";
+            throw new InvalidOperationException(message);
+        }
+
+        return collection.AddScoped<TService>();
+    }
+
+    public static IServiceCollection TryAddScopedOrFail<TService, TImpl>(this IServiceCollection collection)
+        where TService : class
+        where TImpl : class, TService
+    {
+        var descriptors = collection.Where(d => d.ServiceType == typeof(TService)).ToArray();
+        if (descriptors is { Length: > 0 })
+        {
+            var message = $"Service type {typeof(TService).FullName} is already implemented by {Join(", ", descriptors.Select(d => (d.ImplementationType ?? typeof(TService)).FullName))}";
+            throw new InvalidOperationException(message);
+        }
+
+        return collection.AddScoped<TService, TImpl>();
+    }
+
+    public static IServiceCollection TryAddTransientOrFail<TService>(this IServiceCollection collection)
+        where TService : class
+    {
+        var descriptors = collection.Where(d => d.ServiceType == typeof(TService)).ToArray();
+        if (descriptors is { Length: > 0 })
+        {
+            var message = $"Service type {typeof(TService).FullName} is already implemented by {Join(", ", descriptors.Select(d => (d.ImplementationType ?? typeof(TService)).FullName))}";
+            throw new InvalidOperationException(message);
+        }
+
+        return collection.AddTransient<TService>();
+    }
+
+    public static IServiceCollection TryAddTransientOrFail<TService, TImpl>(this IServiceCollection collection)
+        where TService : class
+        where TImpl : class, TService
+    {
+        var descriptors = collection.Where(d => d.ServiceType == typeof(TService)).ToArray();
+        if (descriptors is { Length: > 0 })
+        {
+            var message = $"Service type {typeof(TService).FullName} is already implemented by {Join(", ", descriptors.Select(d => (d.ImplementationType ?? typeof(TService)).FullName))}";
+            throw new InvalidOperationException(message);
+        }
+
+        return collection.AddTransient<TService, TImpl>();
+    }
+
+
 }
---
+++ FSharpExtensions/En3Tho.FSharp.Extensions.Tests/DependencyInjectionTests/ServiceCollectionExtensionsTests.fs
@@ -0,0 +1,26 @@
+﻿module En3Tho.FSharp.Extensions.Tests.DependencyInjectionTests.ServiceCollectionExtensionsTests
+
+open System
+open En3Tho.Extensions.DependencyInjection
+open Microsoft.Extensions.DependencyInjection
+open Xunit
+
+type IMyType = interface end
+type MyType() =
+    interface IMyType
+
+[<Fact>]
+let ``Test that try add methods properly work``() =
+    let serviceCollection = ServiceCollection()
+    serviceCollection.TryAddSingletonOrFail<IMyType, MyType>() |> ignore
+    Assert.Throws<InvalidOperationException>(fun() ->
+        serviceCollection.TryAddSingletonOrFail<IMyType, MyType>() |> ignore)
+    |> ignore
+
+    Assert.Throws<InvalidOperationException>(fun() ->
+        serviceCollection.TryAddScopedOrFail<IMyType, MyType>() |> ignore)
+    |> ignore
+
+    Assert.Throws<InvalidOperationException>(fun() ->
+        serviceCollection.TryAddTransientOrFail<IMyType, MyType>() |> ignore)
+    |> ignore
