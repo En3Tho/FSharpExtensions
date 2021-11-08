fbc98acf0f2f3fbfcee55ab3e1f8b78d7d6c223b
En3Tho <37334640+En3Tho@users.noreply.github.com>
2021-11-08 14:06:30 +0300

    Added vtask, unitvtask, unittask builders TODO: option/result task and vtask builders

--- FSharpExtensions/Benchmarks/Program.fs
+++ FSharpExtensions/Benchmarks/Program.fs
@@ -1,7 +1,10 @@
 open BenchmarkDotNet.Running
 open Benchmarks
 
+open En3Tho.FSharp.ComputationExpressions.Tasks
+
 [<EntryPoint>]
 let main argv =
-    BenchmarkRunner.Run<ScanfBenchmark.Benchmark>() |> ignore
+
+    BenchmarkRunner.Run<TaskBuildersBenchmarks.Benchmark>() |> ignore
     0 // return an integer exit code
---
+++ FSharpExtensions/En3Tho.FSharp.ComputationExpressions/TaskBuilders/TaskBuilders.fs
@@ -0,0 +1,34 @@
+﻿[<AutoOpen>]
+module En3Tho.FSharp.ComputationExpressions.Tasks.TaskBuilders
+
+[<assembly: AutoOpen("En3Tho.FSharp.ComputationExpressions.Tasks.UnitTaskBuilderExtensions.LowPriority")>]
+[<assembly: AutoOpen("En3Tho.FSharp.ComputationExpressions.Tasks.UnitTaskBuilderExtensions.MediumPriority")>]
+[<assembly: AutoOpen("En3Tho.FSharp.ComputationExpressions.Tasks.UnitTaskBuilderExtensions.HighPriority")>]
+
+[<assembly: AutoOpen("En3Tho.FSharp.ComputationExpressions.Tasks.UnitValueTaskBuilderExtensions.LowPriority")>]
+[<assembly: AutoOpen("En3Tho.FSharp.ComputationExpressions.Tasks.UnitValueTaskBuilderExtensions.MediumPriority")>]
+[<assembly: AutoOpen("En3Tho.FSharp.ComputationExpressions.Tasks.UnitValueTaskBuilderExtensions.HighPriority")>]
+
+[<assembly: AutoOpen("En3Tho.FSharp.ComputationExpressions.Tasks.ValueTaskBuilderExtensions.LowPriority")>]
+[<assembly: AutoOpen("En3Tho.FSharp.ComputationExpressions.Tasks.ValueTaskBuilderExtensions.MediumPriority")>]
+[<assembly: AutoOpen("En3Tho.FSharp.ComputationExpressions.Tasks.ValueTaskBuilderExtensions.HighPriority")>]
+
+#if VALUE_OPTION_BUILDER
+
+[<assembly: AutoOpen("En3Tho.FSharp.ComputationExpressions.Tasks.ValueTaskValueOptionBuilderExtensions.LowPriority")>]
+[<assembly: AutoOpen("En3Tho.FSharp.ComputationExpressions.Tasks.ValueTaskValueOptionBuilderExtensions.MediumPriority")>]
+[<assembly: AutoOpen("En3Tho.FSharp.ComputationExpressions.Tasks.ValueTaskValueOptionBuilderExtensions.HighPriority")>]
+
+#endif
+
+do()
+
+let unittask = UnitTaskBuilder()
+let vtask = ValueTaskBuilder()
+let unitvtask = UnitValueTaskBuilder()
+
+#if VALUE_OPTION_BUILDER
+
+let voptionvtask = ValueTaskValueOptionBuilder()
+
+#endif
