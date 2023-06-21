bb7032f066da54081e916949c5617658b076e248
En3Tho- <razerael@gmail.com>
2023-06-21 10:17:49 +0300

    cpm

--- FSharpExtensions/En3Tho.FSharp.Extensions.Tests/DeferTests.fs
+++ FSharpExtensions/En3Tho.FSharp.Extensions.Tests/DeferTests.fs
@@ -3,6 +3,7 @@ module En3Tho.FSharp.Extensions.Tests.DeferTests
 open System.Threading.Tasks
 open Xunit
 open En3Tho.FSharp.Extensions
+open En3Tho.FSharp.ComputationExpressions.Tasks
 
 [<Fact>]
 let ``Test that defer properly works with both disposable and iasyncdisposable`` () = task {
@@ -24,7 +25,38 @@ let ``Test that defer properly works with both disposable and iasyncdisposable``
         let disposeAsync = fun() -> &disposeCounter +<- 1; ValueTask()
         let disposevAsync = fun _ -> &disposeCounter +<- 1; ValueTask()
 
-        use _ = defer disposeAsync
+        use _ = Defer.defer disposeAsync
+        use _ = (fun() -> &disposeCounter +<- 1; ValueTask()) |> defer
+        use _ = defer (fun() -> &disposeCounter +<- 1; ValueTask())
+        use _ = 1 |> deferv disposevAsync
+        use _ = deferv disposevAsync 1
+        ()
+    }
+
+    Assert.Equal(12, disposeCounter)
+}
+
+[<Fact>]
+let ``Test that defer properly works with both disposable and iasyncdisposable2`` () = vtask {
+    let mutable disposeCounter = 0
+
+    do! task {
+        let dispose = fun() -> &disposeCounter +<- 1
+        let disposev = fun _ -> &disposeCounter +<- 1
+
+        use _ = defer dispose
+        use _ = (fun() -> &disposeCounter +<- 1) |> defer
+        use _ = defer (fun() -> &disposeCounter +<- 1)
+        use _ = 1 |> deferv disposev
+
+        use _ = defer (fun() -> &disposeCounter +<- 1)
+        use _ = 1 |> deferv (fun _ -> &disposeCounter +<- 1)
+        use _ = deferv (fun _ -> &disposeCounter +<- 1) 1
+
+        let disposeAsync = fun() -> &disposeCounter +<- 1; ValueTask()
+        let disposevAsync = fun _ -> &disposeCounter +<- 1; ValueTask()
+
+        use _ = Defer.defer disposeAsync
         use _ = (fun() -> &disposeCounter +<- 1; ValueTask()) |> defer
         use _ = defer (fun() -> &disposeCounter +<- 1; ValueTask())
         use _ = 1 |> deferv disposevAsync
