9994a9c8146b52d2dd078ab81afb88f83a9ecfec
En3Tho- <razerael@gmail.com>
2023-11-18 19:47:38 +0300

    few more tests for cancellable task prototype

--- FSharpExtensions/En3Tho.FSharp.ComputationExpressions.Tests/CancellableTaskTests.fs
+++ FSharpExtensions/En3Tho.FSharp.ComputationExpressions.Tests/CancellableTaskTests.fs
@@ -1,6 +1,7 @@
 ﻿module En3Tho.FSharp.ComputationExpressions.Tests.CancellableTaskTests
 
 open System
+open System.Collections.Generic
 open System.Threading
 open System.Threading.Tasks
 open Xunit
@@ -48,4 +49,67 @@ let ``test that cancellable task propagates cancellation token correctly``() = t
         } :> Task
 
     return! Assert.ThrowsAsync<TaskCanceledException>(f)
+}
+
+[<Fact>]
+let ``Test that disposables will be disposed even if task is canceled``() = task {
+    use cts = CancellationTokenSource()
+    let messages = List()
+
+    let cancellable = cancellableTask (cts.Token) {
+        use _ = { new IDisposable with member _.Dispose() = messages.Add("Disposed") } // should be there
+        cts.Cancel()
+        use _ = { new IAsyncDisposable with
+                    member _.DisposeAsync() =
+                        uvtask {
+                            do! Task.Delay(50)
+                            messages.Add("DisposedAsync")
+                        } } // should be there
+        try
+            Console.WriteLine("Try")
+        finally
+            Console.WriteLine("Finally")
+    }
+
+    let f() =
+        task {
+            let! _ = cancellable
+            ()
+        } :> Task
+
+    let! _ = Assert.ThrowsAsync<OperationCanceledException>(f)
+    Assert.Equal(2, messages.Count)
+    Assert.Equal("DisposedAsync", messages[0])
+    Assert.Equal("Disposed", messages[1])
+}
+
+[<Fact>]
+let ``Test that disposables will be disposed even if task is canceled 2``() = task {
+    use cts = CancellationTokenSource()
+    let messages = List()
+
+    let cancellable = cancellableTask (cts.Token) {
+        cts.Cancel()
+        use _ = { new IDisposable with member _.Dispose() = messages.Add("Disposed") } // should be there
+        use _ = { new IAsyncDisposable with
+                    member _.DisposeAsync() =
+                        uvtask {
+                            do! Task.Delay(50)
+                            messages.Add("DisposedAsync")
+                        } } // should be there
+        try
+            Console.WriteLine("Try")
+        finally
+            Console.WriteLine("Finally")
+    }
+
+    let f() =
+        task {
+            let! _ = cancellable
+            ()
+        } :> Task
+
+    let! _ = Assert.ThrowsAsync<OperationCanceledException>(f)
+    Assert.Equal(1, messages.Count)
+    Assert.Equal("Disposed", messages[0])
 }
